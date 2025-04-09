352007d9e6ab32700660c46996cb49e604084914
Meir Elbaz <meire@flow-sec.com>
2025-04-09 10:31:48 +0300

    update tables

--- clickhouse/init.sql
+++ clickhouse/init.sql
@@ -55,17 +55,9 @@ ENGINE = Null();
 
 CREATE TABLE IF NOT EXISTS flows
 (
-    type Int32,
-    time_received DateTime64(9),
-    sequence_num UInt32,
-    sampling_rate UInt64,
-
-    time_inserted DateTime64(9),
+    time_flow_start DateTime,
 
     sampler_address LowCardinality(String),
-
-    time_flow_start DateTime64(9),
-    time_flow_end DateTime64(9),
 
     bytes UInt64,
     packets UInt64,
@@ -74,44 +66,52 @@ CREATE TABLE IF NOT EXISTS flows
     dst_addr String,
 
     etype UInt16,
-
     proto UInt8,
+    tcp_flags UInt16,
 
     src_port UInt16,
     dst_port UInt16,
 
-    in_if UInt32,
-    out_if UInt32,
-
-    src_mac String,
-    dst_mac String,
+    src_as UInt32,
 
     forwarding_status UInt32,
-    tcp_flags UInt16,
-    icmp_type UInt16,
-    icmp_code UInt16,
-
-    fragment_id UInt32,
-    fragment_offset UInt32,
-
-    src_as UInt32,
-    dst_as UInt32,
-
-    src_net UInt8,
-    dst_net UInt8,
-
-    next_hop String,
-    next_hop_as UInt32,
-
-    bgp_next_hop String,
-
-    observation_domain_id UInt32,
-    observation_point_id UInt32
 )
 ENGINE = MergeTree()
-PARTITION BY toDate(time_received)
-ORDER BY time_received
-TTL toDate(time_received) + INTERVAL 30 DAY;
+PARTITION BY toDate(time_flow_start)
+ORDER BY time_flow_start
+TTL toDate(time_flow_start) + INTERVAL 30 DAY;
 
 CREATE MATERIALIZED VIEW flows_mv TO flows AS
-    SELECT *, now() AS time_inserted FROM flows_sink;
+    SELECT
+        toStartOfMinute(time_flow_start) AS time_flow_start,
+        sampler_address,
+
+        sum(bytes) AS bytes,
+        sum(packets) AS packets,
+
+        src_addr,
+        dst_addr,
+
+        etype,
+        proto,
+        tcp_flags,
+
+        src_port,
+        dst_port,
+
+        src_as,
+
+        forwarding_status
+    FROM flows_sink
+    GROUP BY 
+        time_flow_start,
+        sampler_address,
+        src_addr,
+        dst_addr,
+        etype,
+        proto,
+        tcp_flags,
+        src_port,
+        dst_port,
+        src_as,
+        forwarding_status;
