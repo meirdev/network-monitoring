4d5d327aa3ce37a9c50c4074abe9472174dbcf42
Meir Elbaz <meire@flow-sec.com>
2025-05-29 17:38:15 +0300

    add api

---
+++ api/main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/ClickHouse/clickhouse-go/v2"
+	"github.com/ClickHouse/clickhouse-go/v2/lib/driver"
+	"github.com/gin-gonic/gin"
+)
+
+var conn driver.Conn
+
+func GetTop(c *gin.Context) {
+	column := c.Query("column")
+	if column == "" {
+		c.JSON(400, gin.H{"error": "column parameter is required"})
+		return
+	}
+
+	var query string
+
+	k := c.Query("k")
+
+	chCtx := clickhouse.Context(context.Background(), clickhouse.WithParameters(clickhouse.Parameters{
+		"column": column,
+		"k":      k,
+	}))
+
+	if k == "" {
+		query = `
+		WITH top AS (
+			SELECT {column:Identifier} AS column, sum(bytes) AS bytes
+			FROM flows_raw
+			GROUP BY column
+			HAVING bytes > 0
+			ORDER BY bytes DESC
+		)
+		SELECT toString(column) AS column, bytes
+		FROM top
+		`
+	} else {
+		query = `
+		WITH top_k AS (
+			SELECT arrayJoin(approx_top_sum({k:UInt32})({column:Identifier}, bytes)) AS i
+			FROM flows_raw
+		)
+		SELECT toString(i.1) AS column, i.2 AS bytes
+		FROM top_k
+		`
+	}
+
+	rows, err := conn.Query(chCtx, query)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+	defer rows.Close()
+
+	var results []struct {
+		Column string `ch:"column"`
+		Bytes  uint64 `ch:"bytes"`
+	}
+	for rows.Next() {
+		var result struct {
+			Column string `ch:"column"`
+			Bytes  uint64 `ch:"bytes"`
+		}
+		if err := rows.Scan(&result.Column, &result.Bytes); err != nil {
+			c.Error(err)
+			return
+		}
+		results = append(results, result)
+	}
+	if err := rows.Err(); err != nil {
+		c.Error(err)
+		return
+	}
+	c.JSON(200, results)
+}
+
+func main() {
+	var err error
+
+	conn, err = clickhouse.Open(&clickhouse.Options{
+		Addr: []string{"localhost:9000"},
+		Auth: clickhouse.Auth{
+			Database: "default",
+			Username: "default",
+			Password: "password",
+		},
+		TLS: nil,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	if err := conn.Ping(context.Background()); err != nil {
+		log.Fatal(err)
+	}
+
+	router := gin.Default()
+	router.GET("/top", GetTop)
+	router.Run(":8090")
+}
