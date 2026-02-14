fb4edc6bfe74cae19ebf9628921c6a7aebfc18ea
meirdev <maornet@gmail.com>
2026-02-14 21:40:02 +0200

    fix the clickhouse url

--- app/nm/settings.py
+++ app/nm/settings.py
@@ -1,3 +1,4 @@
+from pydantic.fields import Field
 from pydantic import ClickHouseDsn
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
@@ -5,8 +6,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", extra="allow")
 
-    clickhouse_dsn_admin: ClickHouseDsn
-    clickhouse_dsn_reader: ClickHouseDsn
+    clickhouse_dsn_admin: ClickHouseDsn = Field(..., env="CLICKHOUSE_DSN_ADMIN")
+    clickhouse_dsn_reader: ClickHouseDsn = Field(..., env="CLICKHOUSE_DSN_READER")
 
 
 settings = Settings()
